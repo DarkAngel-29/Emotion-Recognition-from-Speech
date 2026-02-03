d62f942d3529f5ab7e44fd633f3ff6fc9d671e29
DarkAngel-29 <dineshchitra29@gmail.com>
2026-02-03 23:23:28 +0530

    Day2 Mfcc deep understanding

--- src/extract_mfcc.py
+++ src/extract_mfcc.py
@@ -1,13 +1,25 @@
 import librosa
 import matplotlib.pyplot as plt
+import numpy as np
 
 audio_path = librosa.ex('trumpet')
 signal, sr = librosa.load(audio_path)
 
 mfcc = librosa.feature.mfcc(y=signal, sr=sr, n_mfcc=13)
-print(mfcc.shape)
 
+mfcc_norm = (mfcc - np.mean(mfcc)) / np.std(mfcc)
+
+mfcc_norm = mfcc_norm.T
+
+##Later i will load when needed
+##mfcc = np.load("data/features/sample_mfcc.npy")
+
+np.save("data/features/sample_mfcc.npy", mfcc_norm)
+
+print(mfcc_norm.shape)
+
+"""
 plt.imshow(mfcc, aspect='auto', origin='lower')
 plt.title("MFCC")
 plt.colorbar()
-plt.show()
+plt.show()"""
